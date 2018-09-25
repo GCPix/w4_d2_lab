c51636fc87aeed65df2ded291078397ebaf74f4b
GillianC <gccc0309@gmail.com>
2018-09-25 16:26:13 +0100

    updating files without css

---
+++ pizza_controller.rb
@@ -0,0 +1,53 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative('./models/pizza_order') #to require the whole models folder write it as ./models/*'
+
+also_reload('..models/*') #this is so that we don't have to stop and start the web server (sinatra)
+
+#index route, all the pizzas
+get '/pizza-orders' do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+#new a pizza
+#put full routes before partial routes with placeholders ie pizza_holder/new is a filepath, :id is placeholder.  if i had an option to search by name and id I would need to specifiy the filepath seperately for each ie pizza_orders/fine_by_name/:name and pizza_orders/find_by_id/:id
+get '/pizza-orders/new' do
+
+  erb(:new)
+end
+
+#show an individual order
+get '/pizza-orders/:id' do
+
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+get '/pizza-orders/:id/edit' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:edit)
+end
+#create
+post '/pizza-orders' do
+  #as the class is initialized as a hash it just needs to have the inforamtion passed across we just pass the full hash
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+end
+
+post '/pizza-orders/:id' do
+  @order = PizzaOrder.new(params)
+#we are using the .new(params) because the params, including the id is being passed from the webserver.  At initialisation this creates the object, the object can then be used to update the database
+  @order.update()
+  erb(:update)
+end
+
+post	'/pizza-orders/:id/delete' do
+  @order = PizzaOrder.find(params[:id])
+
+  @order.delete()
+  erb(:delete)
+end
